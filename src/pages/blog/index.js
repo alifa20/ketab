13f783be447cc533069c806ce275ced46bfdf899
Ali <ali.fathieh@gmail.com>
2020-06-17 21:43:21 +1000

    some changes.

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -22,7 +22,7 @@ export default class BlogIndexPage extends React.Component {
               padding: "1rem",
             }}
           >
-            Latest Stories 2
+            Latest Stories 3
           </h1>
         </div>
         <section className="section">
