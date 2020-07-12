e676670f6b3cffe8acd0191a7190e1793999752c
Ali <ali.fathieh@gmail.com>
2020-07-12 23:41:06 +1000

    book post added.

---
+++ src/templates/book-post.js
@@ -0,0 +1,39 @@
+import { graphql } from "gatsby";
+import PropTypes from "prop-types";
+import React from "react";
+
+export const BookPageTemplate = () => <div>This is Bookpage</div>;
+
+const BookPage = ({ data }) => {
+  const { allMarkdownRemark: post } = data;
+
+  return null;
+};
+
+BookPage.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+export default BookPage;
+
+export const BookPageQuery = graphql`
+  query BookPage {
+    allMarkdownRemark(
+      limit: 1000
+      filter: { frontmatter: { templateKey: { eq: "category-post" } } }
+    ) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            tags
+            templateKey
+          }
+        }
+      }
+    }
+  }
+`;
