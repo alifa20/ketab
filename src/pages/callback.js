69068636f1041c007383914003e417dcfec8dbec
ketab.au <ketab.aus.it@gmail.com>
2020-09-21 23:19:48 +1000

    deploy issue fixed.

--- src/pages/callback.js
+++ src/pages/callback.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { handleAuthentication } from "../utils/auth";
+import { isBrowser } from "../utils/auth";
 
 const Callback = () => {
   handleAuthentication();
+  if (!isBrowser) return <p>Loading...</p>;
+
   const storage = localStorage.getItem("isLoggedIn");
 
   return <p>Loading...</p>;
